0c548da5e55ab3c95116d0eb040e45a24d9534cc
herodote2242 <matthias.gil@hotmail.fr>
2017-11-17 23:00:46 +0100

    modification and improvement according to mentor's comments on structure.py and mc_gyver.py

--- fichiers-mc-gyver/mc_gyver.py
+++ fichiers-mc-gyver/mc_gyver.py
@@ -1,61 +1,106 @@
 #!/usr/bin/python3
 # -*- coding: Utf-8 -*
 
-from structure import *
-from gardien import *
+import structure
+import gardien
+
 
 
 class Mc_gyver:
 	
+	"""
+	creation of the class Mc_Gyver, allowing to define itself and its movement method
+	"""
+
 	def __init__(self, structure):
 		self.case_x = 1
 		self.case_y = 1
 		self.structure = structure
 
 
-	def moves(self, direction):
+	def count_objects():
+		pass
+		#to be replaced by une function when objects implemented
+
+
+	def move(self, direction):
 		"""movements of the main character in the maze"""
+		
+		def is_wall():
+			"""
+			verification of the existence of a wall allowing or not the movement
+			"""
+			if self.structure[self.case_x][self.case_y] == "#":
+				is_wall(self.structure) = True
+
+			else:
+				is_wall(self.structure) = False
+
+
+		def is_guardian():
+			"""
+			verification of the presence of the guardian
+			"""
+			if self.structure[self.case_x][self.case_y] == "g":
+				is_guardian(self.structure) = True
+
+			else:
+				is_guardian(self.structure) = False
+
+
+
+		end = False
+		is_wall = False
+		is_guardian = False
+
+
 		if direction == 'right':
-			if self.structure[self.case_y][self.case_x+1] != '#':
+			if is_wall(self.structure[self.case_y][self.case_x+1]) = False:
 				self.case_x += 1
 
-		if direction == 'left':
-			if self.structure[self.case_y][self.case_x-1] != '#':
+		elif direction == 'left':
+			if is_wall(self.structure[self.case_y][self.case_x-1]) = False:
 				self.case_x -= 1
 
-		if direction == 'up':
-			if self.structure[self.case_y-1][self.case_x] != '#':
+		elif direction == 'up':
+			if is_wall(self.structure[self.case_y-1][self.case_x]) = False:
 				self.case_y -= 1
 
-		if direction == 'down':
-			if self.structure[self.case_y+1][self.case_x] != '#':
+		elif direction == 'down':
+			if is_wall(self.structure[self.case_y+1][self.case_x]) = False:
 				self.case_y += 1
 
-		if Mc_gyver.self.case_x, Mc_gyver.self.case_y = 14, 14:
+		if is_guardian(self.structure[self.case_x][self.case_y]) = True:
 			end = True
+
+	
 
 
 def main():
-	#loop of the game : Mc Gyver is moving until the exit
+	"""
+	loop of the game : Mc Gyver is moving until the exit
+	"""
+
+	Mc_gyver = Mc_gyver(structure)
 
 	end = False
 
 	while not end:
 		#to be replaced by "for event in pygame.event.get()" when importing pygame
-		response = input("where do you want to go?")
+		response = input("Where do you want to go? left, right, up, down ? Or quit ?)
 
-		if response in ["left", "right", "up", "down"]:
-			Mc_gyver.moves(response)
+		if response in ["left", "right", "up", "down", "quit"]:
+			end = Mc_gyver.move(response)
 
 		# up to date of the maze when pygame imported:
 		# pygame.display.flip()
 		# otherwise :
 		maze.display()
 
-	if Mc_gyver.self.case_x, Mc-gyver.self.case_y = 14, 14:
+	if is_guardian(self.structure[self.case_x][self.case_y]) = True:
 		print("You win !")
 		#add an automatic function "exit window"
 
-def count_objects():
-	pass
-	#to be replaced by une function when objects implemented
+
+
+main()
--- fichiers-mc-gyver/structure.py
+++ fichiers-mc-gyver/structure.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 # -*- coding: Utf-8 -*
 
-from objets import *
-from mc_gyver import *
-from gardien import *
 import json
 
 
 class Maze:
 	"""
-	a rectangular maze made of 15 sprites width and 15 sprites height
+	a square maze made of 15 sprites width and 15 sprites height
 	"""
 
 	def __init__(self):
-	    # sprites are displayed according to blanks or # in structure_modifiable.py
+	    """
+	    sprites are displayed according to blanks or # in structure_modifiable.py
+	    """
 	    with open('structure_modifiable.json', 'r') as f:
 	    	self.data = json.load(f)
 
 
 	def display(self):
+		"""
+		method allowing the .json (wich can be easily and-modified) to
+		be displayed as a maze structure 15/15
+		"""
 		for line in self.data:
 			print("".join(line))
 
